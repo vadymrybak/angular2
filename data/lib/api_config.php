51468aefdf1456399f03251473ce4b735b3e0628
vrybak <vrybak@researchnow.com>
2018-05-24 17:21:12 -0400

    dev2

--- data/lib/api_config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-
-//--------------------DECIPHER SERVER TOKENS--------------------\\
-$instance = array("d" => array("key" => "w4tace16r1j6f9fujk10zu5c7ayu86gqbmw21haw1phs64yx57t6j5fm2yky76ur"),
-				  "ca" => array("key" => "0jab58x5jn2rf1jcg5u8wv60zm6tz1g2x6ghccmu093zar94cwxv06kfhj52cg9e"),
-				  "au" => array("key" => "nznaayqm41n6merdek4eav0dd8zpxjzt0bg7y98hjtvktxub95fe8mjpsbk7zpup "),
-				  "uk" => array("key" => "abkr1v9wdqye1wth4jddsk5qnt039cjk9w238d8z98jx25mmmdbzatkm4afsgvx1"));
- ?>
--- data/lib/gen_config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-date_default_timezone_set('America/New_York');
-
-// Base link where files will be stored. Needs to be modified if API transfered to different server.
-$baseLink = "http://surveytools-dev.researchnow.com/vrybak/getdata/new/";
-
- ?>
